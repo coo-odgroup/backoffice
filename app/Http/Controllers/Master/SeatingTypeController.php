ae688a3e50c94de8fc8836ce53bd2c73dfa9753a
sahilprogrammer <sahil.odbus@gmail.com>
2026-02-19 17:10:56 +0530

    seating type module done

--- app/Http/Controllers/Master/SeatingTypeController.php
+++ app/Http/Controllers/Master/SeatingTypeController.php
@@ -3,12 +3,229 @@
 namespace App\Http\Controllers\Master;
 
 use App\Http\Controllers\Controller;
+use App\Models\Master\BusType;
+use App\Models\Master\Districts;
+use App\Models\Master\SeatType;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
 
 class SeatingTypeController extends Controller
 {
- public function seatingType()
+    public function seatingType()
     {
         return view('master.seatingtype');
+    }
+
+    public function dataTableView()
+    {
+        $recordsTotal     = 0;
+        $recordsFiltered  = 0;
+        $data             = [];
+
+        try {
+
+            $txtSearch = htmlEncode(request('txtSearch'));
+            $selStatus = (request('selStatus') !== null && request('selStatus') !== '') ? (int)request('selStatus') : '';
+
+            $dataQuery = DB::table('mst_seat_type as s')
+                ->leftJoin('users as u', 'u.id', '=', 's.created_by')
+                ->select(
+                    's.id as seat_type_id',
+                    's.seat_type',
+                    's.created_at',
+                    's.created_by',
+                    'u.name as created_by_name',
+                    's.active_status'
+                );
+
+            // Filters
+            if (!empty($txtSearch)) {
+                $dataQuery->where(function ($q) use ($txtSearch) {
+                    $q->where('s.seat_type', 'like', "%{$txtSearch}%");
+                });
+            }
+
+            if (isset($selStatus) && $selStatus != '') {
+                $dataQuery->where('s.active_status', $selStatus);
+            }
+
+            $count = $dataQuery->count('s.id');
+
+            $start  = request()->input('start', 0);
+            $length = request()->input('length', 10);
+
+            $start  = is_numeric($start) ? (int)$start : 0;
+            $length = is_numeric($length) ? (int)$length : 10;
+
+            // Ordering
+            if (!empty(request('order'))) {
+
+                $columns = [2 => 's.seat_type', 3 => 's.created_at', 4 => 's.created_by', 5 => 's.active_status'];
+
+                $orderBy       = request('order');
+                $orderColumn   = $columns[$orderBy[0]['column']] ?? 's.seat_type';
+                $orderType     = $orderBy[0]['dir'];
+            } else {
+                $orderColumn = 's.seat_type';
+                $orderType   = 'asc';
+            }
+
+            $dataQuery = $dataQuery->orderBy($orderColumn, $orderType);
+
+            // Pagination
+            if ($length == -1) {
+                $arrRes = $dataQuery->get();
+            } else {
+                $arrRes = $dataQuery->limit($length)
+                    ->offset($start)
+                    ->get();
+            }
+            // Format Data
+            if (count($arrRes) > 0) {
+
+                foreach ($arrRes as $val) {
+                    $val->created_date  = date('d-M-Y H:i:s', strtotime($val->created_at));
+                    $val->is_active     = ($val->active_status == 1) ? 'Active' : 'Inactive';
+                    $val->enc_seat_type_id   = Crypt::encryptString($val->seat_type_id);
+                }
+            }
+
+            $recordsTotal     = $count;
+            $recordsFiltered  = $count;
+            $data             = $arrRes;
+        } catch (\Throwable $t) {
+
+            Log::info("Exception occurred in SeatingTypeController@dataTableView", [
+                'error_message' => $t->getMessage(),
+                'trace' => $t->getTraceAsString()
+            ]);
+
+            $errorMsg = config('constants.SERVER_ERROR_MESSAGE');
+
+            Log::error("Error", [
+                'Controller' => 'SeatingTypeController',
+                'Method'     => 'dataTableView',
+                'Error'      => $errorMsg
+            ]);
+
+            $recordsTotal     = 0;
+            $recordsFiltered  = 0;
+            $data            = [];
+        }
+
+        return response()->json([
+            'recordsTotal'    => $recordsTotal,
+            'recordsFiltered' => $recordsFiltered,
+            'data'            => $data,
+        ]);
+    }
+
+    public function add($encId = null)
+    {
+
+        $data = [];
+        $data['strPage'] = $method = 'Add';
+        $data['strSubmit'] = 'Submit';
+        $data['strReset'] = 'Reset';
+
+        try {
+
+            $id = (!empty($encId)) ? Crypt::decryptString($encId) : 0;
+
+            if ($id > 0) {
+
+                $redirectPage = "admin/seatingtype/edit/" . $encId;
+                $data['strPage'] = $method = 'Edit';
+                $data['strSubmit'] = 'Update';
+                $data['strReset'] = 'Cancel';
+
+                $dataResQry = SeatType::select('id', 'seat_type');
+
+                $dataResQry = $dataResQry->where('id', $id)->first();
+
+                if (empty($dataResQry)) {
+                    return redirect("seatingtype");
+                }
+                $data['row'] = $dataResQry;
+            } else {
+                $id = 0;
+                $redirectPage = "admin/seatingtype";
+            }
+
+            if (request()->isMethod('post')) {
+
+                request()->replace(request()->all());
+
+                $validator = Validator::make(request()->all(), [
+                    'txtSeatType' => 'bail|required'
+                ], [
+                    'txtSeatType.required' => 'Seat Type cannot be left blank.'
+                ]);
+
+                if ($validator->fails()) {
+                    return back()->withErrors($validator)->withInput();
+                } else {
+                    DB::beginTransaction();
+
+                    $txtSeatType  = htmlEncode(request('txtSeatType'));
+
+                    $duplicate = SeatType::select('id')->where(['seat_type' => $txtSeatType]);
+
+                    // return $txtSeatType;exit;
+
+                    if ($id != 0) {
+                        $duplicate->where('id', '!=', $id);
+                    }
+
+                    if ($duplicate->exists()) {
+                        return back()->with([
+                            'level'     => 'danger',
+                            'message'   => 'Seat Type already exist'
+                        ])->withInput();
+                    } else {
+                        $obj = ($id != 0) ? SeatType::find($id) : new SeatType();
+                        $obj->seat_type = $txtSeatType;
+                        $obj->created_by = 1;
+                        $obj->active_status = 1;
+
+                        if ($id != 0) {
+                            $obj->updated_by = 1;
+                        }
+
+                        $obj->save();
+
+                        session()->flash('level', 'success');
+                        session()->flash('message', 'Seat Type ' . (($id != 0) ? 'updated' : 'created') . ' successfully.');
+                    }
+
+                    DB::commit();
+                    return redirect($redirectPage);
+                }
+            }
+        } catch (\Throwable $t) {
+            Log::error("Error", [
+                'Controller' => 'SeatingTypeController',
+                'Method'     => $method,
+                'Error'      => $t->getMessage()
+            ]);
+
+            DB::rollBack();
+
+            $errorMsg = config('constants.SERVER_ERROR_MESSAGE');
+
+            return back()->with([
+                'level'     => 'danger',
+                'message'   => $errorMsg
+            ])->withInput();
+        }
+        return view('Master.addSeatingtype', compact('data'));
+    }
+
+    public function edit($encId)
+    {
+        return $this->add($encId);
     }
 }
---
+++ resources/views/Master/addSeatingtype.blade.php
@@ -0,0 +1,146 @@
+@extends('admin.layouts.master')
+@section('content')
+
+<?php
+$page_name = 'All Cities';
+$listButtons = ['indicate' => 'N', 'print' => 'N', 'xls' => 'N', 'download' => 'N', 'back' => 'N', 'delete' => 'y', 'active' => 'y', 'inactive' => 'y'];
+?>
+
+
+<!-- Breadcrumb -->
+<nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="#">Home</a></li>
+        <li class="breadcrumb-item">Master</li>
+        <li class="breadcrumb-item active">{{ $data['strPage'] }} Seat Type</li>
+    </ol>
+</nav>
+
+<!-- Booking Report Card -->
+<!-- HEADER -->
+<div class="d-flex justify-content-between align-items-center mb-2">
+    <h5 id="page_title">Seat Types</h5>
+    <div>
+        <a class="btn btn-success btn-sm" href="{{ route('seatingtype.index') }}">View Seat Types
+        </a>
+    </div>
+</div>
+
+<!-- TABLE -->
+<form id="backoffice-form" name="backoffice-form" method="post" novalidate class="w-100 add-cities-form">
+    {{csrf_field()}}
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+                    <!-- FILTER -->
+                    <div class="mb-3">
+                        <div class="card-body">
+                            <div class="row">
+                                @if (session('message'))
+
+                                <div class="alert alert-{{ session('level') ?? 'success' }} alert-dismissible fade show" role="alert">
+                                    {{ session('message') }}
+                                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                                </div>
+
+                                @endif
+                                @if ($errors->any())
+                                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                    <ul class="mb-0">
+                                        @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                        @endforeach
+                                    </ul>
+
+                                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                                </div>
+                                @endif
+
+                                <!-- FILTER FIELDS -->
+                                <div class="col-12">
+                                    <div class="row mb-3">
+                                        <div class="col-md-12 mb-3">
+                                            <label for="txtSeatType">Seat Type<span class="text-danger important">*</span></label>
+                                            <input type="text" class="form-control" id="txtSeatType" name="txtSeatType" value="{{ $data['row']->seat_type ?? '' }}" placeholder="Enter Seat Type">
+                                        </div>
+                                    </div>
+
+                                </div>
+
+                                <!-- BUTTONS -->
+                                <div class="row mt-4">
+                                    <div class="col-12 d-flex gap-2 justify-content-md-start justify-content-center">
+                                        <button class="btn btn-primary btn-sm" type="submit">
+                                            {{ $data['strSubmit'] }}
+                                        </button>
+                                        @if($data['strReset'] == 'Cancel')
+                                        <a class="btn btn-secondary btn-sm" type="button" href="{{ route('district.index') }}">
+                                            {{ $data['strReset'] }}
+                                        </a>
+                                        @else
+                                        <button class="btn btn-secondary btn-sm" id="btnReset" type="button">
+                                            {{ $data['strReset'] }}
+                                        </button>
+                                        @endif
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+</form>
+
+@endsection
+@push('scripts')
+
+<script type="module">
+    // document.getElementById('txtDistrict').addEventListener('input', function() {
+
+    // });
+
+
+    // $(document).ready(function() {
+
+    //     commonAjax.initSelect2('#selState', 'Select State');
+
+    //     let state_id = <?= $data['row']->state_id ?? '0' ?>
+
+    //     commonAjax.loadStateList(state_id);
+    // });
+
+    $('#btnReset').click(function() {
+        $(':input', '#backoffice-form').not(':button, :submit, :reset, :hidden').val('');
+        $('.form-select').val(0);
+        $('.form-select').val('').trigger('change');
+    });
+
+    $('#backoffice-form').on('submit', function(e) {
+
+        e.preventDefault();
+        if (!validator.blankCheck('txtSeatType', 'Seat Type cannot be left blank'))
+            return false;
+
+        if (!validator.maxLength('txtSeatType', 100, 'Seat Type'))
+            return false;
+
+        commonAjax.confirmAlert('Are you sure to proceed !');
+
+        $('#btnConfirmOk').on('click', function() {
+            e.currentTarget.submit();
+        });
+
+    });
+
+    document.getElementById("menu-toggle").addEventListener("click", function() {
+        document.getElementById("sidebar-wrapper").classList.toggle("collapsed");
+    });
+</script>
+
+
+@endpush
