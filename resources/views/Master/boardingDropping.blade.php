324821679c957027853a7436b0ac8bf6584c69f5
subhasis mohanty <subhasis.odbus@gmail.com>
2026-02-21 12:23:43 +0530

    Boarding Droping log

--- resources/views/Master/boardingDropping.blade.php
+++ resources/views/Master/boardingDropping.blade.php
@@ -145,8 +145,7 @@ $listButtons = ['indicate' => 'N', 'print' => 'N', 'xls' => 'N', 'download' => '
                         <th>Type</th>
                         <th>Boarding/Dropping Point</th>
                         <th width="100">Sequence No</th>
-                        <th>Created By</th>
-                        <th>Created On</th>
+                        <th>Last Modefied</th>
                         <th>Status</th>
                         <th class="no-sort">Action</th>
                     </tr>
@@ -286,14 +285,40 @@ $listButtons = ['indicate' => 'N', 'print' => 'N', 'xls' => 'N', 'download' => '
                 },
                 defaultContent: "--"
             },
+ {
+                data: null,
+                render: function (data, type, row) {
 
-            {
-                data: 'created_by_name',
-                defaultContent: "--"
-            },
-            {
-                data: 'created_date',
-                defaultContent: "--"
+                    let createdBy  = row.created_by_name ?? '--';
+                    let createdAt  = row.created_date ?? '--';
+
+                    let updatedBy  = row.updated_by_name ? row.updated_by_name : '--';
+                    let updatedAt  = (row.updated_date) ? row.updated_date : '--';
+                 
+                    // Show updated date if exists, else created date
+                    let shortDate = row.updated_date
+                        ? row.updated_date.split(' ')[0]
+                        : (createdAt !== '--' ? createdAt.split(' ')[0] : '--');
+
+                    return `
+                        <span
+                            class="text-decoration-underline fw-semibold"
+                            data-bs-toggle="tooltip"
+                            data-bs-placement="top"
+                            data-bs-html="true"
+                            title="
+                                <div class='audit-box'>
+                                    <div><strong>Created By:</strong> ${createdBy}</div>
+                                    <div><strong>Created At:</strong> ${createdAt}</div>
+                                    <hr class='my-1'>
+                                    <div><strong>Updated By:</strong> ${updatedBy}</div>
+                                    <div><strong>Updated At:</strong> ${updatedAt}</div>
+                                </div>
+                            ">
+                            ${createdAt}
+                        </span>
+                    `;
+                }
             },
             {
                 data: 'is_active',
@@ -315,6 +340,13 @@ $listButtons = ['indicate' => 'N', 'print' => 'N', 'xls' => 'N', 'download' => '
                         <a class="btn btn-sm btn-info"
                         href="${editUrl.replace('ID', row.enc_bd_id)}">
                         <i class="fa fa-edit"></i> Edit
+                        </a>
+
+                         <a href="javascript:void(0);"
+                            class="btn btn-sm btn-success btn-view-log"
+                            data-table="mst_cities"
+                            data-id="${row.enc_bd_id}">
+                                <i class="fa fa-history"></i> View Log
                         </a>
                     `;
                 },
