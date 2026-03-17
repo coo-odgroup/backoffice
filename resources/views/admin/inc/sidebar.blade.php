37034ea8ff9d3e1ac1d2bfd2412f397bfa81fc71
Jagannath Das <jagandas.odbus@gmail.com>
2026-03-17 14:51:38 +0530

    sidebar updated

--- resources/views/admin/inc/sidebar.blade.php
+++ resources/views/admin/inc/sidebar.blade.php
@@ -21,13 +21,13 @@
         <a class="list-group-item list-group-item-action d-flex justify-content-between align-items-center"
             data-bs-toggle="collapse"
             href="#busManagement"
-            aria-expanded="{{ Request::is('admin/states*','admin/district*','admin/cities*','admin/bustype*','admin/seatingtype*','admin/amenitycategory*','admin/amenities*','admin/roles*','admin/reason*','admin/modules*','admin/boardingDropping*','admin/apiapps*','admin/apikeys*','admin/cityapis*','admin/users*','admin/brand*','admin/bus-model*','admin/axle-type*','admin/bus-service*','admin/mst-seatlayout*','admin/annexture-type  *') ? 'true' : 'false' }}">
+            aria-expanded="{{ Request::is('admin/states*','admin/district*','admin/cities*','admin/bustype*','admin/seatingtype*','admin/amenitycategory*','admin/amenities*','admin/roles*','admin/reason*','admin/modules*','admin/boardingDropping*','admin/apiapps*','admin/apikeys*','admin/cityapis*','admin/users*','admin/brand*','admin/bus-model*','admin/axle-type*','admin/bus-service*','admin/mst-seatlayout*','admin/annexture-type*','admin/cancellationslab*','admin/cancellationslab-info*') ? 'true' : 'false' }}">
             <span><i class="fa-solid fa-bus me-2"></i> Bus Management</span>
             <i class="fa-solid fa-chevron-down small"></i>
         </a>
 
         <!-- Sub Menu -->
-        <div class="collapse {{ Request::is('admin/states*','admin/district*','admin/cities*','admin/bustype*','admin/seatingtype*','admin/amenitycategory*','admin/amenities*','admin/roles*','admin/reason*','admin/modules*','admin/boardingDropping*','admin/apiapps*','admin/apikeys*','admin/cityapis*','admin/users*','admin/brand*','admin/bus-model*','admin/axle-type*','admin/bus-service*','admin/mst-seatlayout*','admin/annexture-typet*') ? 'show' : '' }}" id="busManagement">
+        <div class="collapse {{ Request::is('admin/states*','admin/district*','admin/cities*','admin/bustype*','admin/seatingtype*','admin/amenitycategory*','admin/amenities*','admin/roles*','admin/reason*','admin/modules*','admin/boardingDropping*','admin/apiapps*','admin/apikeys*','admin/cityapis*','admin/users*','admin/brand*','admin/bus-model*','admin/axle-type*','admin/bus-service*','admin/mst-seatlayout*','admin/annexture-type*','admin/cancellationslab*','admin/cancellationslab-info*') ? 'show' : '' }}" id="busManagement">
 
             <a href="{{ url('admin/brand') }}"
                 class="list-group-item list-group-item-action ps-4 {{ Request::is('admin/brand*') ? 'active' : '' }}">
@@ -147,6 +147,16 @@
             <a href="{{ url('admin/reviewcategory') }}"
                 class="list-group-item list-group-item-action ps-4 {{ Request::is('admin/reviewcategory*') ? 'active' : '' }}">
                 <i class="fa-solid fa-star me-2"></i> Review Category
+            </a>
+
+            <a href="{{ url('admin/cancellationslab') }}"
+                class="list-group-item list-group-item-action ps-4 {{ Request::is('admin/cancellationslab*') ? 'active' : '' }}">
+                <i class="fa-solid fa-star me-2"></i> Cancellation Slab
+            </a>
+
+            <a href="{{ url('admin/cancellationslab-info') }}"
+                class="list-group-item list-group-item-action ps-4 {{ Request::is('admin/cancellationslab-info*') ? 'active' : '' }}">
+                <i class="fa-solid fa-star me-2"></i> Cancellation Slab Info
             </a>
 
         </div>
